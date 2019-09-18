14d93c742f7b3988a9cddf49c34066ef720493aa
Leon-Na <55507344+Leon-Na@users.noreply.github.com>
2019-09-18 15:13:41 -0400

    Add files via upload

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Lin Na, A student comes from China, is a 19 - year - old boy, which totally fine if anyone want to call me Leon. ");
+            Console.WriteLine("What I hope to get form this class is to have a basic view coding word and ability of code. ");
+            Console.WriteLine("I became interest in computer because my dad was watching the tv all the day, and I don't have any other option to watch cartoons. ");
+            Console.WriteLine("This opened the door of the Internet to me. ");
+            Console.WriteLine("The programing for me is how people express the funtion that they want coumputer to do.I would like to figure out how to do it. ");
+            Console.WriteLine("I would like to transfer to another University to learn somehow more. One of my goal is to be rich$, and I wanna to learn more new languages,include the computer languages. ");
+            Console.WriteLine("I only had learned the HTML for one semester.");
+            Console.ReadKey();
+        }
+    }
+}
